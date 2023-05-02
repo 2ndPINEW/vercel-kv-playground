74055847052e93b633ae12bafcbc7fd4bdce4f88
Eito Takase <eito01178@gmail.com>
2023-05-02 17:14:05 +0900

    classNameいらない

--- components/say-hello.tsx
+++ components/say-hello.tsx
@@ -3,9 +3,7 @@ import kv from "@vercel/kv";
 export default async function SayHello({ name }: { name: string }) {
   const kvCache = await kv.json.get(cacheKey(name), "$");
   if (kvCache) {
-    return (
-      <p className="text-sm text-gray-500">{kvCache[0].message} from kv</p>
-    );
+    return <p>{kvCache[0].message} from kv</p>;
   }
 
   const res = await fetch(
@@ -14,8 +12,7 @@ export default async function SayHello({ name }: { name: string }) {
   const { message } = await res.json();
 
   await kv.json.set(cacheKey(name), "$", { message });
-
-  return <p className="text-sm text-gray-500">{message} from api</p>;
+  return <p>{message} from api</p>;
 }
 
 function cacheKey(name: string) {
