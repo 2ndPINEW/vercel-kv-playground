5773e6e181a6993715bdb8146aae8185b7ef4025
Eito Takase <eito01178@gmail.com>
2023-05-02 17:56:12 +0900

    kvKey

--- components/say-hello.tsx
+++ components/say-hello.tsx
@@ -1,7 +1,7 @@
 import kv from "@vercel/kv";
 
 export default async function SayHello({ name }: { name: string }) {
-  const kvCache = await kv.json.get(cacheKey(name), "$");
+  const kvCache = await kv.json.get(kvKey(name), "$");
   if (kvCache) {
     return <p>{kvCache[0].message} from kv</p>;
   }
@@ -11,10 +11,10 @@ export default async function SayHello({ name }: { name: string }) {
   );
   const { message } = await res.json();
 
-  await kv.json.set(cacheKey(name), "$", { message });
+  await kv.json.set(kvKey(name), "$", { message });
   return <p>{message} from api</p>;
 }
 
-function cacheKey(name: string) {
-  return `${name}-cache`;
+function kvKey(name: string) {
+  return `${name}-key`;
 }
